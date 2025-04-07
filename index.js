7d7d47e26d61e97965dd85653ad86ad0e0943649
TheDrawingTotems <mitvin1947@students.uplifteducation.org>
2025-04-07 18:15:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+console.log("idndex.js loaded")
 
+const canvas = document.getElementById("canvas").inner
+const ctx = canvas.getContext("2d")
+
+
+
+function update(){
+  ctx.fillRect(0, 0, 50, 50)
+  reqestAnimationFrame(update)
+}
+update()
