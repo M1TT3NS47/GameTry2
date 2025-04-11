ce277404145ca08dd32638c9ca5ebdfb33c95a67
TheDrawingTotems <mitvin1947@students.uplifteducation.org>
2025-04-10 21:56:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ console.log("idndex.js loaded")
 const canvas = document.getElementById("canvas").inner
 const ctx = canvas.getContext("2d")
 
-
+let x = 0;
+let y = 0;
 
 function update(){
   ctx.fillRect(0, 0, 50, 50)
